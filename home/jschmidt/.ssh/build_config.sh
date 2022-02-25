baeb6d51313090cc0e8f443de95744562626773c
Jeff Schmidt <jeff.schmidt@zenith.com>
2022-02-25 01:02:23 -0600

    major changes. bash to zsh, modularize pam, sudo user adds, ssh

--- home/jschmidt/.ssh/build_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -vex
-DATE=$(date +%s)
-cd ~/.ssh/
-cp config config_backup-${DATE}
-cat config.base >  config
-cat include.d/* >> config
-\diff -du config_backup-${DATE} config && \
-  rm config_backup-${DATE}
